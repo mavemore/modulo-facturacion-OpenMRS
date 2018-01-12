dcf3db31acd77ef6dacd7a6d8e9f9bb45da7f781
odmoreno <oscarpol95@gmail.com>
2018-01-11 20:04:53 -0500

    cambios

--- app/js/components/servicios/serviceList.jsx
+++ app/js/components/servicios/serviceList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../global/Header';
 import ReactTable from 'react-table';
-import 'react-table/react-table.css'
+import 'react-table/react-table.css';
 
 const data = [{
     id: 'P001',
@@ -12,7 +12,7 @@ const data = [{
 }]
 
 const columns = [{
-    Header: 'Codigo',
+    Header: 'Sodigo',
     accesor: 'id'
 },{
     Header: 'Insumo',
@@ -32,10 +32,19 @@ export default class serviceList extends React.Component{
     render(){
         return(
             <div>
-                <h1>Servicios</h1>
+                <h1>SSSSSServicios</h1>
+                <div>
+                    <Link to='/nuevo'><button className="btn">Agregar Servicio</button></Link>
+                </div>
                 <div>
                     <ReactTable
-                        data = {data}
+                        data = {[{
+                            id: 'P001',
+                            nombre: 'Agua Destilada',
+                            precio: 0.39,
+                            cantidad: 1,
+                            medida: 'cc'
+                        }]}
                         columns = {columns}
                     />
                 </div>
