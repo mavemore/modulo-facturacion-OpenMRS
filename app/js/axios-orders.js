c09a6a2659e7555213ec92d61ab35ad964c6d51a
odmoreno <oscarpol95@gmail.com>
2018-02-07 20:30:46 -0500

    changes

--- app/js/axios-orders.js
+++ app/js/axios-orders.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:8080/openmrs/ws/rest/v1',
+    baseURL: 'http://localhost:8081/openmrs-standalone/ws/rest/v1',
+    //baseURL: 'http://localhost:8080/openmrs/ws/rest/v1',
     headers: {'X-Requested-With': 'XMLHttpRequest'},
     auth: {
         username: 'admin',
--- webpack.config.js
+++ webpack.config.js
@@ -183,10 +183,7 @@ var webpackConfig = {
 		}, {
 			test: /\.css$/,
 			loader: 'css-loader',
-			query: {
-				modules : true,
-				localIdentName: '[name]__[local]___[hash:base64:5]'
-			}
+			
 		}, {
 	    test: /\.(png|jpg|jpeg|gif|svg)$/,
 	    loader: 'url'
