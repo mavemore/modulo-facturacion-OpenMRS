1923d410b0b05cc92a3bac2bf07c119c256be38f
mavemore <veronica940413@hotmail.com>
2018-02-08 09:14:58 -0500

    mostrar pacientes

--- app/js/api/serviciosPacientes.jsx
+++ app/js/api/serviciosPacientes.jsx
@@ -1,11 +1,19 @@
 import {instance} from '../axios-orders';
 
-export function getPaciente(uuid)
+function getPaciente(uuid)
 {
     return instance.get('/person/'+uuid);
 }
 
-export function buscarPaciente(query)
+function buscarPaciente(query)
 {
     return instance.get('/patient?q='+query);
 }
+
+var helpers = {
+    getPacientes: function(query){
+        return instance.all(buscarPaciente(query))
+        .then(function(res){
+    return {res}
+        })
+}}
